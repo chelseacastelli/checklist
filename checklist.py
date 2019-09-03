08cdb7e0b180d801c4b8ed4fd128ba37263eafeb
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-03 07:43:26 -0700

    Added color and clear functions for easier readability

--- checklist.py
+++ checklist.py
@@ -1,3 +1,7 @@
+import os
+from subprocess import call
+from colorama import Fore, Back, Style
+
 checklist = list()
 
 # CREATE
@@ -14,14 +18,9 @@ def update(index, item):
 
 # DESTROY
 def destroy(index):
-    # From @umarill on
-    # https://discuss.codecademy.com/t/how-can-i-check-if-an-index-is-valid/377316/3
-    if checkForItem(index):
-        checklist.pop(int(index))
-    else:
-        print("There is no item at this index\n")
+    checklist.pop(int(index))
 
-def checkForItem(index):
+def item_exists(index):
     try:
         checklist[int(index)]
         return True
@@ -33,21 +32,40 @@ def list_all_items():
     print('\n')
     # Output for empty list
     index = 0
-    checkForItem(index)
-    for list_item in checklist:
-        print("{} {}".format(index, list_item))
-        index += 1
+    if item_exists(index):
+        for list_item in checklist:
+            item = color_text(list_item)
+            print("{} {}".format(index, item))
+            index += 1
+    else:
+        print("The list is empty")
+
+def color_text(list_item):
+    if list_item.upper() == "RED":
+        return Fore.RED + list_item + Style.RESET_ALL
+    elif list_item.upper() == "ORANGE":
+        return Fore.ORANGE + list_item + Style.RESET_ALL
+    elif list_item.upper() == "YELLOW":
+        return Fore.YELLOW + list_item + Style.RESET_ALL
+    elif list_item.upper() == "GREEN":
+        return Fore.GREEN + list_item + Style.RESET_ALL
+    elif list_item.upper() == "BLUE":
+        return Fore.BLUE + list_item + Style.RESET_ALL
+    elif list_item.upper() == "PURPLE":
+        return Fore.MAGENTA + list_item + Style.RESET_ALL
+    else:
+        return Style.RESET_ALL + list_item + Style.RESET_ALL
 
 def mark_complete(index):
     # Error handle (invalid user input)
-    if checkForItem(index):
-        checklist[int(index)] = u'\u2713' + ' ' + checklist[int(index)]
+    if item_exists(index):
+        item = checklist[int(index)]
+        checklist[int(index)] = u'\u2713' + ' ' + color_text(item)
     else:
         print("There is no item at this index\n")
 
-    list_all_items()
-
 def select(function_code):
+    clear()
     # Create item
     if function_code == "A":
         input_item = user_input("Input item: ")
@@ -55,14 +73,19 @@ def select(function_code):
 
     # Read item
     elif function_code == "R":
-        item_index = user_input("Index Number? ")
+        list_all_items()
+        item_index = user_input("\nIndex Number? ")
         # Remember that item_index must actually exist or our program will crash.
-        destroy(item_index)
+        if item_exists(item_index):
+            destroy(item_index)
+        else:
+            print("There is no item at this index\n")
 
     # Update item
     elif function_code == "U":
-        item_index = user_input("Index Number? ")
-        if checkForItem(item_index):
+        list_all_items()
+        item_index = user_input("\nIndex Number? ")
+        if item_exists(item_index):
             new_item_name = user_input("New item name: ")
             update(item_index, new_item_name.upper())
         else:
@@ -70,7 +93,8 @@ def select(function_code):
 
     # Mark complete
     elif function_code == "C":
-        item_index = user_input("Index Number? ")
+        list_all_items()
+        item_index = user_input("\nIndex Number? ")
         mark_complete(item_index)
 
     # List all items
@@ -90,6 +114,9 @@ def select(function_code):
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
+
+def clear():
+    _ = call('clear' if os.name =='posix' else 'cls')
 
 # TEST
 def test():
